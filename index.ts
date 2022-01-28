e990489f1fa8dbe02ee924828ae33c0e83a80230
Utsavkumar Lal <utsav.lal@makkajai.com>
2022-01-28 11:09:03 +0530

    Reset back to 10 events

--- index.ts
+++ index.ts
@@ -50,7 +50,7 @@ interface TransformationsMap {
 }
 
 
-const EVENTS_PER_BATCH = 100
+const EVENTS_PER_BATCH = 10
 const REDIS_OFFSET_KEY = 'import_offset'
 
 const sanitizeSqlIdentifier = (unquotedIdentifier: string): string => {
